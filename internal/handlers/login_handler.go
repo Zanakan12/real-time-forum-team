09e3cef2aa8b0ba8fe208b3d84eabd2a2cce5335
kimig3 <kimigame3@gmail.com>
2025-03-11 01:40:05 +0100

    modification of the file for connected js to golang

--- internal/handlers/login_handler.go
+++ internal/handlers/login_handler.go
@@ -1,10 +1,15 @@
 package handlers
 
 import (
-	"html/template"
+	"encoding/json"
 	"net/http"
 	"net/url"
 )
+
+type LoginResponse struct {
+	Error   string `json:"error"`
+	Message string `json:"message"`
+}
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	// Récupérer le message d'erreur s'il existe
@@ -12,32 +17,14 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	if errorMsg != "" {
 		errorMsg, _ = url.QueryUnescape(errorMsg)
 	}
-	// Parse the templates from files.
-	tmpl, err := template.ParseFiles(
-		"web/pages/login.html",
-		"web/templates/tmpl_login.html",
-		"web/templates/tmpl_nav.html",
-		"web/templates/tmpl_message_usr_ban.html",
-	)
-	if err != nil {
-		http.Error(w, "Internal Server Error (Error parsing templates)", http.StatusInternalServerError)
-		return
+
+	// Construire la réponse JSON
+	response := LoginResponse{
+		Error:   errorMsg,
+		Message: "Veuillez entrer vos identifiants",
 	}
 
-	// Assuming this is an empty login form
-	loginData := LoginPage{
-		Nav:               NavData,
-		Title:             "Forum | Login",
-		NotificationCount: 0,
-		UserRole:          "traveler",
-		Login:             LoginData,
-		CurrentPage: 	   "login",
-	}
-	loginData.Login.Error = errorMsg
-	// Execute the template and pass any data needed (nil here for simplicity).
-	err = tmpl.Execute(w, loginData)
-	if err != nil {
-		http.Error(w, "Internal Server Error (Error executing template)", http.StatusInternalServerError)
-		return
-	}
+	// Envoyer la réponse en JSON
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
 }
